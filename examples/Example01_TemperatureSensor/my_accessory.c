5204b75010aa800308788968574eab920350c887
Mixiaoxiao(谜小小) <xiaochyechye@gmail.com>
2020-05-15 02:25:57 +0800

    Example01_TemperatureSensor

---
+++ examples/Example01_TemperatureSensor/my_accessory.c
@@ -0,0 +1,82 @@
+/*
+ * my_accessory.c
+ * Define the accessory in C language using the Macro in characteristics.h
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ */
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+
+// Called to identify this accessory. See HAP section 6.7.6 Identify Routine
+// Generally this is called when paired successfully or click the "Identify Accessory" button in Home APP.
+void my_accessory_identify(homekit_value_t _value) {
+	printf("accessory identify\n");
+}
+
+// For TEMPERATURE_SENSOR,
+// the required characteristics are: CURRENT_TEMPERATURE
+// the optional characteristics are: NAME, STATUS_ACTIVE, STATUS_FAULT, STATUS_TAMPERED, STATUS_LOW_BATTERY
+// See HAP section 8.41 and characteristics.h
+
+// (required) format: float; HAP section 9.35; min 0, max 100, step 0.1, unit celsius
+homekit_characteristic_t cha_current_temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+
+// (optional) format: string; HAP section 9.62; max length 64
+homekit_characteristic_t cha_name = HOMEKIT_CHARACTERISTIC_(NAME, "Temperature Sensor");
+
+
+// (optional) format: bool; HAP section 9.96
+// homekit_characteristic_t cha_status_active = HOMEKIT_CHARACTERISTIC_(STATUS_ACTIVE, true);
+
+// (optional) format: uint8; HAP section 9.97; 0 "No Fault", 1 "General Fault"
+// homekit_characteristic_t cha_status_fault = HOMEKIT_CHARACTERISTIC_(STATUS_FAULT, 0);
+
+// (optional) format: uint8; HAP section 9.100; 0 "Accessory is not tampered", 1 "Accessory is tampered with"
+// homekit_characteristic_t cha_status_tampered = HOMEKIT_CHARACTERISTIC_(STATUS_TAMPERED, 0);
+
+// (optional) format: uint8; HAP section 9.99; 0 "Battery level is normal", 1 "Battery level is low"
+// homekit_characteristic_t cha_status_low_battery = HOMEKIT_CHARACTERISTIC_(STATUS_LOW_BATTERY, 0);
+
+// example for humidity
+// homekit_characteristic_t cha_humidity  = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            &cha_current_temperature,
+			&cha_name,//optional
+			//&cha_status_active,//optional
+			//&cha_status_fault,//optional
+			//&cha_status_tampered,//optional
+			//&cha_status_low_battery,//optional
+            NULL
+        }),
+		// Add this HOMEKIT_SERVICE if you has a HUMIDITY_SENSOR together
+		/*
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &cha_humidity,
+            NULL
+        }),*/
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+		.accessories = accessories,
+		.password = "111-11-111"
+};
+
+
---
+++ examples/Example01_TemperatureSensor/temperature_sensor.ino
@@ -0,0 +1,97 @@
+/*
+ * temperature_sensor.ino
+ *
+ * This example shows how to:
+ * 1. define a temperature sensor accessory and its characteristics (in my_accessory.c).
+ * 2. report the sensor value to HomeKit (just random value here, you need to change it to your real sensor value).
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ *
+ * Note:
+ *
+ * You are recommended to read the Apple's HAP doc before using this library.
+ * https://developer.apple.com/support/homekit-accessory-protocol/
+ *
+ * This HomeKit library is mostly written in C,
+ * you can define your accessory/service/characteristic in a .c file,
+ * since the library provides convenient Macro (C only, CPP can not compile) to do this.
+ * But it is possible to do this in .cpp or .ino (just not so conveniently), do it yourself if you like.
+ * Check out homekit/characteristics.h and use the Macro provided to define your accessory.
+ *
+ * Generally, the Arduino libraries (e.g. sensors, ws2812) are written in cpp,
+ * you can include and use them in a .ino or a .cpp file (but can NOT in .c).
+ * A .ino is a .cpp indeed.
+ *
+ * You can define some variables in a .c file, e.g. int my_value = 1;,
+ * and you can access this variable in a .ino or a .cpp by writing extern "C" int my_value;.
+ *
+ * So, if you want use this HomeKit library and other Arduino Libraries together,
+ * 1. define your HomeKit accessory/service/characteristic in a .c file
+ * 2. in your .ino, include some Arduino Libraries and you can use them normally
+ *                  write extern "C" homekit_characteristic_t xxxx; to access the characteristic defined in your .c file
+ *                  write your logic code (eg. read sensors) and
+ *                  report your data by writing your_characteristic.value.xxxx_value = some_data; homekit_characteristic_notify(..., ...)
+ * done.
+ */
+
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#include "wifi_info.h"
+//include the Arduino library for your real sensor here, e.g. <DHT.h>
+
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+
+void setup() {
+	Serial.begin(115200);
+	wifi_connect(); // in wifi_info.h
+	my_homekit_setup();
+}
+
+void loop() {
+	my_homekit_loop();
+	delay(10);
+}
+
+//==============================
+// Homekit setup and loop
+//==============================
+
+// access your homekit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_current_temperature;
+
+static uint32_t next_heap_millis = 0;
+static uint32_t next_report_millis = 0;
+
+void my_homekit_setup() {
+	arduino_homekit_setup(&config);
+}
+
+void my_homekit_loop() {
+	arduino_homekit_loop();
+	const uint32_t t = millis();
+	if (t > next_report_millis) {
+		// report sensor values every 10 seconds
+		next_report_millis = t + 10 * 1000;
+		my_homekit_report();
+	}
+	if (t > next_heap_millis) {
+		// show heap info every 5 seconds
+		next_heap_millis = t + 5 * 1000;
+		LOG_D("Free heap: %d, HomeKit clients: %d",
+				ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+	}
+}
+
+void my_homekit_report() {
+	float temperature_value = random_value(10, 30); // FIXME, read your real sensor here.
+	cha_current_temperature.value.float_value = temperature_value;
+	LOG_D("Current temperature: %.1f", temperature_value);
+	homekit_characteristic_notify(&cha_current_temperature, cha_current_temperature.value);
+}
+
+int random_value(int min, int max) {
+	return min + random(max - min);
+}
