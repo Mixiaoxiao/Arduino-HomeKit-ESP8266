8a8fd7b9afec4a00449da770470012814912d1f9
Mixiaoxiao (Wang Bin) <xiaochyechye@gmail.com>
2021-03-30 23:30:42 +0800

    Add `yield()` while crypto computing.

--- src/wolfcrypt/src/integer.c
+++ src/wolfcrypt/src/integer.c
@@ -3531,6 +3531,11 @@ int s_mp_exptmod (mp_int * G, mp_int * X, mp_int * P, mp_int * Y, int redmode)
   bitbuf = 0;
 
   for (;;) {
+    /* For ESP8266,
+     * call `yield()` to run WiFi task,
+     * to prevent WiFi disconnection while heavy crypto computing.
+     */
+	yield();
     /* grab next digit as required */
     if (--bitcnt == 0) {
       /* if digidx == -1 we are out of digits */
