fbae363d050d720cda3d05f7ad1f3a6ec962dac0
Ronny23826 <54706797+Ronny23826@users.noreply.github.com>
2021-09-15 03:46:35 +0200

    Ronn

--- examples/Example02_Switch/wifi_info.h
+++ examples/Example02_Switch/wifi_info.h
@@ -14,8 +14,8 @@
 #include <WiFi.h>
 #endif
 
-const char *ssid = "your-ssid";
-const char *password = "your-password";
+const char *ssid = "MeineBox";
+const char *password = "Ronny23826?";
 
 void wifi_connect() {
 	WiFi.persistent(false);
